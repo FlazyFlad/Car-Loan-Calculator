762f6336407f9ab333fb11e35ca994e06ed59023
FlazyFlad <igorpak1337@gmail.com>
2023-11-09 14:46:37 +0600

    Header responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Link } from "react-router-dom";
 import './Header.css';
 
 const Header = ({ isLoggedIn, onLogout }) => {
+
     const handleLogout = () => {
         localStorage.setItem('isLoggedIn', false);
         onLogout();
     };
+
+
+    const [menuActive, setMenuActive] = useState(false);
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 768) {
+                setMenuActive(true);
+            } else {
+                setMenuActive(false);
+            }
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleMenuToggle = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    const links = [
+        { id: 1, text: 'Home', url: '/' },
+        { id: 2, text: 'Loan Calculator', url: '/loan-calculator' },
+        { id: 3, text: 'About Us', url: '/about-us' },
+        // Добавьте четвертую ссылку только если пользователь вошел в систему
+        ...(isLoggedIn
+            ? [{ id: 4, text: 'Profile', url: '/profile' }]
+            : []
+        )
+        // Добавьте другие ссылки по мере необходимости
+    ];
 
     return (
         <>
@@ -15,26 +54,51 @@ const Header = ({ isLoggedIn, onLogout }) => {
                     <div className="logo">
                         <Link to="/"> <i className="fa fa-car" aria-hidden="true"></i> Your Car <span>Loan</span></Link>
                     </div>
-                    <nav>
+
+                <div className="dropdown">
+                {menuActive && (
+                        <div className='menu-bar'>
+                        <i className="fa-solid fa-bars" onClick={handleMenuToggle}></i>
+                        </div>
+                )}
+                {showDropdown && menuActive && (
+
+                    <div className="dropdown-content">
                         <ul>
-                            <li><a href="/">Home</a></li>
+                            {links.map(link => (
+                                <li key={link.id}>
+                                    <Link to={link.url} onClick={handleMenuToggle}>
+                                        {link.text}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-                            <li>
-                                <Link to="/loan-calculator">Loan Calculator</Link>
-                            </li>
+                )}
+                </div>
 
-                            <li>
-                                <Link to="about-us">About Us</Link>
-                            </li>
 
+
+                    <nav>
+                    
+                        <ul>
+                        {!menuActive && (
+                            <>
+                            {links.map(link => (
+                                <li key={link.id}>
+                                    <Link to={link.url}>
+                                        {link.text}
+                                    </Link>
+                                </li>
+                            ))}
+                            </>
+                        )}
                             {isLoggedIn ? (
                                 <>
                                     <li>
-                                        <Link to="/profile">Profile</Link>
-                                    </li>
-                                    <li>
-                                        <Link onClick={handleLogout} class="act-a-link" to="/">
-                                            <button class="act-link">
+                                        <Link onClick={handleLogout} className="act-a-link" to="/">
+                                            <button className="act-link">
                                                 Log Out
                                             </button>
                                         </Link>
@@ -42,8 +106,8 @@ const Header = ({ isLoggedIn, onLogout }) => {
                                 </>
                             ) : (
                                 <li>
-                                    <Link class="act-a-link" to="/register">
-                                        <button class="act-link">
+                                    <Link className="act-a-link" to="/register">
+                                        <button className="act-link">
                                             Sign Up
                                         </button>
                                     </Link>
