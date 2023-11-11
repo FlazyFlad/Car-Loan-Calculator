fa7ff36fa74a8f24cf8be57f9a145d467b28b3ec
FlazyFlad <igorpak1337@gmail.com>
2023-11-11 15:05:51 +0600

    Dark Mode Implementation fixes

--- src/components/LoanCalculator/TitleText.jsx
+++ src/components/LoanCalculator/TitleText.jsx
@@ -1,12 +1,16 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../../Context";
 
 
 
 
 
 const TitleText = ({titleText, subTitleText}) => {
+    const { theme } = useContext(ThemeContext);
+
     return (
         /*Tailwind styles*/
-        <div className='text-center font-bold'>
+        <div className={`text-center font-bold ${theme ? 'dark-text-color' : 'light-color-text'}`}>
             <h3 className="text-2xl">{titleText}</h3>
             <p className="text-xs">{subTitleText}</p>
         </div>
