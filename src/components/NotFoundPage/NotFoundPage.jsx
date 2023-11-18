202d54d578167c91c5f55d7da353a01a40ad13a4
FlazyFlad <igorpak1337@gmail.com>
2023-11-18 16:05:12 +0600

    not found page dark mode

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './NotFoundPage.css'
+import LightThemeCarImage from './404.png'
+import DarkThemeCarImage from './404-dark.png'
+import { ThemeContext } from '../../Context';
 
 const NotFoundPage = () => {
+    
+    const { theme } = useContext(ThemeContext);
+    const backgroundImage = theme ? DarkThemeCarImage : LightThemeCarImage;
+
     return (
         <>
         
-        <div className="notfound-bg">
+        <div className="notfound-bg" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <div className="not-found-container">
                 <h1>Looks like you're lost</h1>
                 <p>The page you are looking for is not available!</p>
